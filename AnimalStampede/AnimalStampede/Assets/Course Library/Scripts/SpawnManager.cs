268538858bc4eae0095b7073d4dc3d59ae141250
Sebastian Greg <fortunatos@roverkids.org>
2025-05-15 14:05:54 -0400

    newer 2.4

--- AnimalStampede/AnimalStampede/Assets/Course Library/Scripts/SpawnManager.cs
+++ AnimalStampede/AnimalStampede/Assets/Course Library/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, 1.5f);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
